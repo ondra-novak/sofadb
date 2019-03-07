c1309b03569e0e0ae3eafe5083126f4d2bfe86d1
Ondrej Novak <ondra-novak@email.cz>
2019-03-08 00:21:01 +0100

    real snapshots for memory storage

--- src/libsofa/kvapi_memdb.cpp
+++ src/libsofa/kvapi_memdb.cpp
@@ -20,7 +20,7 @@ PChangeset MemDB::createChangeset() {
 
 PIterator MemDB::findRange(const std::string_view& prefix,	bool reverse) {
 	Sync _(lock);
-	auto begin = prefix.empty()?data.begin():data.lower_bound(prefix);
+	auto begin = prefix.empty()?data.begin():data.lower_bound(json::StrViewA(prefix));
 	auto end = prefix.empty()?data.end():data.lower_bound(prefixLastKey(prefix));
 	iterCount++;
 	if (reverse) {
@@ -36,8 +36,8 @@ PIterator MemDB::findRange(const std::string_view& prefix,	bool reverse) {
 
 PIterator MemDB::findRange(const std::string_view& start, const std::string_view& stop) {
 	Sync _(lock);
-	auto begin = data.lower_bound(start);
-	auto end = data.lower_bound(stop);
+	auto begin = data.lower_bound(json::StrViewA(start));
+	auto end = data.lower_bound(json::StrViewA(stop));
 	if (start > stop) {
 		return new MemDBIterator<DataMap::reverse_iterator>(
 				std::make_reverse_iterator(begin),
@@ -51,23 +51,27 @@ PIterator MemDB::findRange(const std::string_view& start, const std::string_view
 
 bool MemDB::lookup(const std::string_view& key, std::string& value) {
 	Sync _(lock);
-	auto iter = data.find(key);
+	auto iter = data.find(json::StrViewA(key));
 	if (iter == data.end()) return false;
-	value = iter->second;
-	return true;
+	if (iter->second.valid) {
+		value = iter->second.data.str();
+		return true;
+	} else {
+		return false;
+	}
 }
 
 bool MemDB::exists(const std::string_view& key) {
 	Sync _(lock);
-	auto iter = data.find(key);
+	auto iter = data.find(json::StrViewA(key));
 	return iter != data.end();
 }
 
 bool MemDB::existsPrefix(const std::string_view& key) {
 	Sync _(lock);
-	auto iter = data.lower_bound(key);
+	auto iter = data.lower_bound(json::StrViewA(key));
 	if (iter == data.end()) return false;
-	return std::string_view(iter->second).substr(0,key.length()) == key;
+	return iter->first.substr(0,key.length()) == json::StrViewA(key);
 }
 
 void MemDB::destroy() {
@@ -83,24 +87,12 @@ void MemDBChangeset::put(const std::string_view& key, const std::string_view& va
 void MemDBChangeset::erase(const std::string_view& key) {
 	batchWrite.push_back(Command(2,key));
 }
+void MemDBChangeset::erase(const json::String& key) {
+	batchWrite.push_back(Command(2,key));
+}
 
 void MemDBChangeset::commit() {
-	MemDB::Sync _(memdb->lock);
-	std::string key;
-	for (auto &&c : batchWrite) {
-		switch (c.first) {
-		case 0: std::swap(key, c.second);break;
-		case 1: {
-			std::string &val = memdb->data[std::move(key)];
-			std::swap(val, c.second);
-			break;
-			}
-		case 2: if (memdb->iterCount) memdb->eraseBatch.push_back(std::move(c.second));
-				else memdb->data.erase(c.second);
-				break;
-		}
-	}
-	batchWrite.clear();
+	memdb->commitBatch(batchWrite);
 }
 
 void MemDBChangeset::rollback() {
@@ -109,15 +101,16 @@ void MemDBChangeset::rollback() {
 
 void MemDBChangeset::erasePrefix(const std::string_view& prefix) {
 	MemDB::Sync _(memdb->lock);
-	auto iter = memdb->data.lower_bound(prefix);
-	while (iter != memdb->data.end() && std::string_view(iter->first).substr(0,prefix.length()) == prefix) {
+	json::StrViewA p(prefix);
+	auto iter = memdb->data.lower_bound(json::StrViewA(prefix));
+	while (iter != memdb->data.end() && iter->first.substr(0,prefix.length()) == p) {
 		erase(iter->first);
 		++iter;
 	}
 }
 
-template<typename iterator>
-MemDBIterator<iterator>::MemDBIterator(iterator begin, iterator end, RefCntPtr<MemDB> owner)
+template<typename iterator, typename Owner>
+MemDBIteratorBase<iterator,Owner>::MemDBIteratorBase(iterator begin, iterator end, RefCntPtr<Owner> owner)
 	:begin(begin)
 	,end(end)
 	,owner(owner)
@@ -127,55 +120,236 @@ MemDBIterator<iterator>::MemDBIterator(iterator begin, iterator end, RefCntPtr<M
 
 template<typename iterator>
 MemDBIterator<iterator>::~MemDBIterator() {
-	MemDB::Sync _(owner->lock);
-	if (--owner->iterCount == 0) {
-		for (auto &&c: owner->eraseBatch) {
-			owner->data.erase(c);
+	MemDB::Sync _(this->owner->lock);
+	if (--this->owner->iterCount == 0) {
+		for (auto &&c: this->owner->eraseBatch) {
+			this->owner->data.erase(c);
 		}
-		owner->eraseBatch.clear();
+		this->owner->eraseBatch.clear();
 	}
 }
 
-template<typename iterator>
-bool MemDBIterator<iterator>::getNext(KeyValue& row) {
+template<typename iterator, typename Owner>
+bool MemDBIteratorBase<iterator,Owner>::next() {
 	MemDB::Sync _(owner->lock);
 	if (begin == end) return false;
-	last_value = begin->second;
-	row.first = begin->first;
-	row.second = last_value;
 	++begin;
 	return true;
 }
+template<typename iterator, typename Owner>
+const MemDBCommon::Key &MemDBIteratorBase<iterator,Owner>::getKey() const {
+	return begin->first;
+}
+template<typename iterator, typename Owner>
+const MemDBCommon::Value &MemDBIteratorBase<iterator,Owner>::getValue() const {
+	return begin->second;
+}
+template<typename iterator, typename Owner>
+bool MemDBIteratorBase<iterator,Owner>::hasItems() const {
+	return begin != end;
+}
+
+
+template<typename iterator, typename Owner>
+bool MemDBIteratorBase<iterator,Owner>::getNext(KeyValue& row) {
+	MemDB::Sync _(owner->lock);
+	bool valid;
+	do {
+		if (begin == end) return false;
+		valid = begin->second.valid;
+		last_value = begin->second.data.str();
+		row.first = json::StrViewA(begin->first);
+		row.second = last_value;
+		++begin;
+	}
+	while (!valid);
+	return true;
+}
+
+void MemDB::commitBatch(std::vector<MemDBChangeset::Command> &batch) {
+	MemDB::Sync _(lock);
+	json::String key;
+	for (auto &&c : batch) {
+		switch (c.first) {
+		case 0: std::swap(key, c.second);break;
+		case 1: {
+			MemDB::Value &val = data[key];
+			for (auto &c: snapshots) c->copyOnWrite(key,val);
+			val.data =  c.second;
+			val.valid = true;
+			break;
+			}
+		case 2: {
+			auto iter = data.find(c.second);
+			if (iter != data.end()) {
+				for (auto &c: snapshots) c->copyOnWrite(iter->first,iter->second);
+				if (iterCount) {
+					iter->second.valid = false;
+					iter->second.data = json::String();
+					eraseBatch.push_back(c.second);
+				} else {
+					data.erase(iter);
+				}
+			}
+
+		} break;
+		}
+	}
+	batch.clear();
+
+}
 
 PKeyValueDatabaseSnapshot MemDB::createSnapshot() {
-	return new MemDBSnapshot(this);
+	MemDBSnapshot *msnp = new MemDBSnapshot(this);
+	PKeyValueDatabaseSnapshot snp = msnp;
+	addSnapshot(msnp);
+	return snp;
 }
 
 MemDBSnapshot::MemDBSnapshot(RefCntPtr<MemDB> owner)
-	:owner(owner),lock(owner->lock)
-{
+	:owner(owner){
 
 }
 
 PIterator MemDBSnapshot::findRange(const std::string_view& prefix, bool reverse) {
-	return owner->findRange(prefix,reverse);
+	Sync _(lock);
+	PMemDBIterBase iter1 = PMemDBIterBase::staticCast(owner->findRange(prefix,reverse));
+
+	auto begin = prefix.empty()?data.begin():data.lower_bound(json::StrViewA(prefix));
+	auto end = prefix.empty()?data.end():data.lower_bound(prefixLastKey(prefix));
+
+	if (reverse) {
+		PMemDBIterBase iter2 =  new MemDBIteratorBase<DataMap::reverse_iterator, MemDBSnapshot>(
+				std::make_reverse_iterator(begin),
+				std::make_reverse_iterator(end),
+				this);
+		return new MemDBSnapshotIterator<std::greater<json::String> >(iter1,iter2,std::greater<json::String>());
+	} else {
+		PMemDBIterBase iter2 =  new MemDBIteratorBase<DataMap::iterator, MemDBSnapshot>(begin,end,this);
+		return new MemDBSnapshotIterator<std::less<json::String> >(iter1,iter2,std::less<json::String>());
+	}
+
 }
 
-PIterator MemDBSnapshot::findRange(const std::string_view& start, const std::string_view& end) {
-	return owner->findRange(start,end);
+PIterator MemDBSnapshot::findRange(const std::string_view& start, const std::string_view& stop) {
+	Sync _(lock);
+	PMemDBIterBase iter1 = PMemDBIterBase::staticCast(owner->findRange(start,stop));
+	auto begin = data.lower_bound(json::StrViewA(start));
+	auto end = data.lower_bound(json::StrViewA(stop));
+	if (start > stop) {
+		PMemDBIterBase iter2 = new MemDBIteratorBase<DataMap::reverse_iterator,MemDBSnapshot>(
+				std::make_reverse_iterator(begin),
+				std::make_reverse_iterator(end),
+				this);
+		return new MemDBSnapshotIterator<std::greater<json::String> >(iter1,iter2,std::greater<json::String>());
+	} else {
+		PMemDBIterBase iter2 = new MemDBIteratorBase<DataMap::iterator,MemDBSnapshot>(begin,end,this);
+		return new MemDBSnapshotIterator<std::less<json::String> >(iter1,iter2,std::less<json::String>());
+	}
 }
 
 bool MemDBSnapshot::lookup(const std::string_view& key, std::string& value) {
-	return owner->lookup(key,value);
+	Sync _(lock);
+	auto iter = data.find(json::StrViewA(key));
+	if (iter == data.end()) return owner->lookup(key,value);
+	if (iter->second.valid) {
+		value = iter->second.data.str();
+		return true;
+	} else {
+		return false;
+	}
+
 }
 
 bool MemDBSnapshot::exists(const std::string_view& key) {
-	return owner->exists(key);
+	Sync _(lock);
+	auto iter = data.find(json::StrViewA(key));
+	if (iter != data.end()) {
+		return iter->second.valid;
+	} else {
+		return owner->exists(key);
+	}
+}
+
+MemDBSnapshot::~MemDBSnapshot() {
+	owner->removeSnapshot(this);
 }
 
 bool MemDBSnapshot::existsPrefix(const std::string_view& key) {
-	return owner->existsPrefix(key);
+	Iterator iter ( findRange(key,false));
+	return iter.getNext();
 }
 
+void MemDBSnapshot::copyOnWrite(const Key& key, const Value& value) {
+	Sync _(lock);
+	auto iter = data.find(key);
+	if (iter == data.end()) {
+		data.insert(std::pair(key,value));
+	}
+}
+
+void MemDB::addSnapshot(MemDBSnapshot* snapshot) {
+	Sync _(lock);
+
+	snapshots.push_back(snapshot);
+
+}
+
+void MemDB::removeSnapshot(MemDBSnapshot* snapshot) {
+	Sync _(lock);
+	auto iter = std::find(snapshots.begin(),snapshots.end(),snapshot);
+	if (iter != snapshots.end()) {
+		snapshots.erase(iter);
+	}
+
+}
+
+template< typename Cmp>
+inline MemDBSnapshotIterator<Cmp>::MemDBSnapshotIterator(
+		PIter iter1, PIter iter2, Cmp cmp):iter1(iter1),iter2(iter2),cmp(cmp) {
+}
+
+template<typename Cmp>
+inline bool sofadb::MemDBSnapshotIterator<Cmp>::getNext(KeyValue& row) {
+
+	bool hv1 = iter1->hasItems();
+	bool hv2 = iter2->hasItems();
+	while (hv1 && hv2) {
+		auto k1 = iter1->getKey();
+		auto k2 = iter2->getKey();
+		if (cmp(k1, k2)) {
+			if (iter1->getValue().valid) {
+				iter1->getNext(row);
+				return true;
+			} else {
+				iter1->next();
+			}
+		} else if (cmp(k2 , k1)) {
+			if (iter1->getValue().valid) {
+				iter2->getNext(row);
+				return true;
+			} else {
+				iter2->next();
+			}
+		} else {
+			if (iter2->getValue().valid) {
+				iter2->getNext(row);
+				iter1->next();
+				return true;
+			} else {
+				iter1->next();
+				iter2->next();
+			}
+		}
+	}
+	if (hv1) {
+		return iter1->getNext(row);
+	} else if (hv2) {
+		return iter2->getNext(row);
+	} else {
+		return false;
+	}
+
+}
 
 }
--- src/libsofa/kvapi_memdb.h
+++ src/libsofa/kvapi_memdb.h
@@ -8,6 +8,7 @@
 #ifndef SRC_LIBSOFA_KVAPI_MEMDB_H_
 #define SRC_LIBSOFA_KVAPI_MEMDB_H_
 
+#include <imtjson/string.h>
 #include "kvapi.h"
 #include <map>
 #include <memory>
@@ -28,11 +29,25 @@ public:
 	virtual void commit();
 	virtual void rollback();
 	virtual void erasePrefix(const std::string_view &prefix);
+	void erase(const json::String &key);
+
+	using Command = std::pair<unsigned char,json::String>;
 
 protected:
 	RefCntPtr<MemDB> memdb;
-	using Command = std::pair<unsigned char,std::string>;
 	std::vector<Command> batchWrite;
+};
+
+class MemDBCommon {
+public:
+	using Key = json::String;
+	struct Value {
+		bool valid = false;
+		json::String data;
+	};
+	using Sync = std::unique_lock<std::recursive_mutex>;
+	using DataMap = std::map<Key, Value, std::less<json::StrViewA> >;
+
 };
 
 
@@ -41,23 +56,29 @@ protected:
  * @note we will only emulate snapshot by keeping lock to avoid changes from other threads
  * It expects that snapshot is not kept for long time
  */
-class MemDBSnapshot: public AbstractKeyValueDatabaseSnapshot {
+class MemDBSnapshot: public AbstractKeyValueDatabaseSnapshot, public MemDBCommon {
 public:
 	MemDBSnapshot(RefCntPtr<MemDB> owner);
+	~MemDBSnapshot();
 
 	virtual PIterator findRange(const std::string_view &prefix, bool reverse = false);
 	virtual PIterator findRange(const std::string_view &start, const std::string_view &end);
 	virtual bool lookup(const std::string_view &key, std::string &value);
 	virtual bool exists(const std::string_view &key);
 	virtual bool existsPrefix(const std::string_view &key);
+	void copyOnWrite(const Key &key, const Value &value);
 protected:
 	RefCntPtr<MemDB> owner;
-	std::unique_lock<std::recursive_mutex> lock;
+	std::recursive_mutex lock;
+	DataMap data;
+
+	template<typename,typename> friend class MemDBIteratorBase;
+
 };
 
 
 
-class MemDB: public AbstractKeyValueDatabase {
+class MemDB: public AbstractKeyValueDatabase, public MemDBCommon {
 public:
 	MemDB();
 
@@ -70,45 +91,87 @@ public:
 	virtual void destroy();
 	virtual PKeyValueDatabaseSnapshot createSnapshot();
 
+	void commitBatch(std::vector<MemDBChangeset::Command> &batch);
+
+	void addSnapshot(MemDBSnapshot *snapshot);
+	void removeSnapshot(MemDBSnapshot *snapshot);
 protected:
 
 
-	using DataMap = std::map<std::string, std::string, std::less<> >;
+
 
 	std::recursive_mutex lock;
 	DataMap data;
 	unsigned int iterCount = 0;
-	std::vector<std::string> eraseBatch;
+	std::vector<json::String> eraseBatch;
+	std::vector<MemDBSnapshot *> snapshots;
 
 
 	friend class MemDBChangeset;
 	template<typename> friend class MemDBIterator;
+	template<typename,typename> friend class MemDBIteratorBase;
 	friend class MemDBSnapshot;
 
-	using Sync = std::unique_lock<std::recursive_mutex>;
 };
 
-
-template<typename iterator>
-class MemDBIterator: public AbstractIterator {
+class MemDBIteratorGen: public  AbstractIterator {
 public:
-	MemDBIterator(iterator begin,
-			iterator end,
-			RefCntPtr<MemDB> owner);
+	virtual bool hasItems() const = 0;
+	virtual const MemDBCommon::Key &getKey() const = 0;
+	virtual const MemDBCommon::Value &getValue() const = 0;
+	virtual bool next() = 0;
 
-	~MemDBIterator();
+};
+
+template<typename iterator, typename Owner>
+class MemDBIteratorBase: public MemDBIteratorGen {
+public:
+	MemDBIteratorBase(iterator begin,
+			iterator end,
+			RefCntPtr<Owner> owner);
+
 
 	virtual bool getNext(KeyValue &row);
+	virtual bool hasItems() const ;
+	virtual const MemDBCommon::Key &getKey() const;
+	virtual const MemDBCommon::Value &getValue() const;
+	virtual bool next() ;
 
 protected:
 	iterator begin;
 	iterator end;
-	RefCntPtr<MemDB> owner;
+	RefCntPtr<Owner> owner;
 
 	std::string last_value;
 };
 
 
+
+template<typename iterator>
+class MemDBIterator: public MemDBIteratorBase<iterator,MemDB> {
+public:
+	using MemDBIteratorBase<iterator,MemDB>::MemDBIteratorBase;
+
+	~MemDBIterator();
+};
+
+using PMemDBIterBase =  RefCntPtr<MemDBIteratorGen>;
+
+
+template<typename Cmp>
+class MemDBSnapshotIterator: public AbstractIterator {
+public:
+	using PIter = RefCntPtr<MemDBIteratorGen>;
+	MemDBSnapshotIterator(PIter iter1, PIter iter2, Cmp cmp);
+
+
+	virtual bool getNext(KeyValue &row);
+
+protected:
+	PIter iter1;
+	PIter iter2;
+	Cmp cmp;
+};
 
 }
 
